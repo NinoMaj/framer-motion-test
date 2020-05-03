a0365290a22de201e6788313712e8ac3657aec42
Nino Majder <nino.majder@gmail.com>
2020-05-03 14:01:29 +0200

    Improve Items styles

--- components/Variants.tsx
+++ components/Variants.tsx
@@ -12,6 +12,11 @@ const Items = () => {
       <li>Item 1</li>
       <li>Item 2</li>
       <li>Item 3</li>
+      <style jsx>{`
+        ul {
+          color: #fff;
+        }
+      `}</style>
     </ul>
   )
 }
